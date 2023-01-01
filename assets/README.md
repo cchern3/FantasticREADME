85b14f4e9a5a160349b505a7b679ece47c6e7a20
Calvin Chern <calvin.chern24@gmail.com>
2023-01-01 18:24:46 -0500

    first commits

---
+++ assets/README.md
@@ -0,0 +1,29 @@
+# README Generator
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  ## Description
+  When creating a project on GitHub, a good README is necessary for the application. This would include what the application is about, how to use the application, how to install it, how to report issues, and how to make contributions. This project allow users to generate a README file by using a command-line application to generate one.
+  ## Installation
+  Before attempting to use this project, make sure to have these programs installed on your computer: VS Code, GitBash, and Node.js. Hit the "Code" button within this GitHub repo to copy link Within GitBash, use the command "git clone paste link here.” Use the command "npm init -y" to install Node Package Manager. Then use the command "npm install inquirer" to acquire the npm's inquirer package. 
+  ## Usage
+  Ensure the software is downloaded and Node packages are initialized. Within the directory for this project, initialize prompts using the command "node index.js". Answer and input the text following the information provided within the terminal. A new README file will be generated and placed into the “assets” folder. 
+  ## Licenses
+    This project is covered under the MIT license. Click the license button at the top of the page for more information.
+  ## Contributing
+  Users may make pull requests, submit bugs or feature requests within the updated code if there is something that could be improved for the README generator.
+  ## Tests
+  n/a
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/cchern3  
+  Email: calvin.chern24@gmail.com
+  ## Credits
+  Calvin Chern
---
+++ index.js
@@ -0,0 +1,180 @@
+// Packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter your full name:',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('You must enter a full name to earn credit!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please provide a GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please provide an email address to allow other to contact you if they have questions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter a title for this project:',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please provide a title for the project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Enter your project description here:",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('A description for this project is essential to understand what the project is about. Please provide one!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter intructions for installation:',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions for the installation of this project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter instructions for usage:',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions for usage to allow users to navigate your project appropriately.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please provide guidelines on how others can contribute to your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Give a description on how the tests were written for your project and how to use them:',
+        validate: testsInput => {
+            if (testsInput) {
+                return true;
+            } else {
+                console.log('Please provide a description on how tests were written for the project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLicenses',
+        message: 'Would you like to include a license?',
+        default: false
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'Please select a license:',
+        choices: ['MIT', 'GPL', 'CC--0'],
+        when: ({ confirmLicenses }) => {
+            if (confirmLicenses) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    },
+];
+
+// Function to write README file
+const writeToFile = data => {
+    return new Promise((resolve, reject) => {
+        // make a readme file and add to assets folder
+        fs.writeFile('./assets/README.md', data, err => {
+            // if there's an error, reject the Promise and send the error to .catch() method
+            if (err) {
+                reject (err);
+                return;
+            }
+            // if everything went well, resolve the Promise and send the successful data to the .then() method
+            resolve({
+                ok: true,
+                message: console.log('Success! Navigate to the "assests" folder to see your README!')
+            });
+        })
+    })
+}
+
+// Initialize application
+const init = () => {
+    return inquirer.prompt(questions);
+}
+
+// Function call to initialize app
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+    return writeToFile(readmeInfo);
+})
+.catch(err => {
+    console.log(err);
+})
