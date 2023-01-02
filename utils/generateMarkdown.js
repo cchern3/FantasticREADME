356dcd3379d913d68c0c7bb4e6bbefe601929276
Calvin Chern <calvin.chern24@gmail.com>
2023-01-02 17:11:50 -0500

    demonstration added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -59,6 +59,8 @@ function generateMarkdown(data) {
   ${data.contributing}
   ## Tests
   ${data.tests}
+  ## Demonstration
+  Click this link for a video demonstration: https://drive.google.com/file/d/1yQDwVdattQ0rWfufdhgVp9nhhh5yUY5i/view
   ## Questions
   Have questions about this project?  
   GitHub: https://github.com/${data.github}  
